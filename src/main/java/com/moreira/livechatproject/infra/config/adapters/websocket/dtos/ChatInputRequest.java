41f2e502ed2cf8082796f37afaeb56ed6e0861f7
zenonxd <olavomoreira99@gmail.com>
2024-12-09 16:24:44 -0300

    feat: dto changes

--- src/main/java/com/moreira/livechatproject/infra/config/adapters/websocket/dtos/ChatInputRequest.java
+++ src/main/java/com/moreira/livechatproject/infra/config/adapters/websocket/dtos/ChatInputRequest.java
@@ -1,4 +1,4 @@
 package com.moreira.livechatproject.infra.config.adapters.websocket.dtos;
 
-public record ChatInputRequest(String username, String message) {
+public record ChatInputRequest(String user, String message) {
 }
